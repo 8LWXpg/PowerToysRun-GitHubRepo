2daf624b2857f69f6bef0203eaee4390458147dd
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2024-01-23 21:45:08 +0800

    release script

--- release.ps1
+++ release.ps1
@@ -2,18 +2,18 @@ pushd
 cd $PSScriptRoot
 
 $assembly = 'Community.PowerToys.Run.Plugin.GithubRepo.dll'
-$version = (cat .\plugin.json | ConvertFrom-Json).Version
-
-git tag $version
-git push --tags
+$version = (cat ./plugin.json | ConvertFrom-Json).Version
 
 $archs = @('x64', 'arm64')
+
+rm ./out/* -r -fo -ea ig
+md ./out -ea ig
 foreach($arch in $archs){
-	$releasePath = ".\bin\$arch\Release\net8.0-windows"
+	$releasePath = "./bin/$arch/Release/net8.0-windows"
 
 	dotnet build -c Release /p:Platform=$arch
-	Compress-Archive "$releasePath\$assembly", "$releasePath\plugin.json", "$releasePath\images" ".\GithubRepo_$arch.zip" -Force
+	Compress-Archive "$releasePath/$assembly", "$releasePath/plugin.json", "$releasePath/images" "./out/GithubRepo_$arch.zip" -fo
 }
 
-gh release create $version @(@('x64','ARM64') | % {".\GithubRepo_$_.zip"})
+gh release create $version (ls ./out/*.zip)
 popd
