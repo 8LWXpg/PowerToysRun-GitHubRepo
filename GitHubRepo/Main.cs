69fe2f5cda2febf61454d08988533fb6afbff778
8LWXpg <105704427+8LWXpg@users.noreply.github.com>
2024-07-04 18:52:51 +0800

    Removed result where score < 0 in user repo query

--- GitHubRepo/Main.cs
+++ GitHubRepo/Main.cs
@@ -8,324 +8,267 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using Wox.Infrastructure;
 using Wox.Plugin;
-using Wox.Plugin.Logger;
 using BrowserInfo = Wox.Plugin.Common.DefaultBrowserInfo;
 
-namespace Community.PowerToys.Run.Plugin.GitHubRepo
+namespace Community.PowerToys.Run.Plugin.GitHubRepo;
+
+public partial class Main : IPlugin, IPluginI18n, ISettingProvider, IReloadable, IDisposable, IDelayedExecutionPlugin, IContextMenu
 {
-	public partial class Main : IPlugin, IPluginI18n, ISettingProvider, IReloadable, IDisposable, IDelayedExecutionPlugin, IContextMenu
-	{
-		private static readonly CompositeFormat PluginInBrowserName = CompositeFormat.Parse(Resources.plugin_in_browser_name);
-		private const string DefaultUser = nameof(DefaultUser);
-		private const string AuthToken = nameof(AuthToken);
+	private static readonly CompositeFormat PluginInBrowserName = CompositeFormat.Parse(Resources.plugin_in_browser_name);
+	private const string DefaultUser = nameof(DefaultUser);
+	private const string AuthToken = nameof(AuthToken);
 
-		private string? _iconFolderPath;
-		private string? _iconFork;
-		private string? _iconRepo;
-		private string? _icon;
-		private string? _defaultUser;
-		private string? _authToken;
+	private string? _iconFolderPath;
+	private string? _iconFork;
+	private string? _iconRepo;
+	private string? _icon;
+	private string? _defaultUser;
+	private string? _authToken;
 
-		private CachingService? _cache;
+	private CachingService? _cache;
 
-		// additional data for context menu
-		private record ResultData(string Url);
+	// additional data for context menu
+	private record ResultData(string Url);
 
-		private PluginInitContext? _context;
+	private PluginInitContext? _context;
 
-		private bool _disposed;
+	private bool _disposed;
 
-		public string Name => Resources.plugin_name;
+	public string Name => Resources.plugin_name;
 
-		public string Description => Resources.plugin_description;
+	public string Description => Resources.plugin_description;
 
-		public static string EmptyDescription => Resources.plugin_empty_description;
+	public static string EmptyDescription => Resources.plugin_empty_description;
 
-		public static string PluginID => "47B63DBFBDEE4F9C85EBA5F6CD69E243";
+	public static string PluginID => "47B63DBFBDEE4F9C85EBA5F6CD69E243";
 
-		public IEnumerable<PluginAdditionalOption> AdditionalOptions =>
-		[
-			new()
-			{
-				PluginOptionType = PluginAdditionalOption.AdditionalOptionType.Textbox,
-				Key = DefaultUser,
-				DisplayLabel = Resources.plugin_default_user,
-				TextBoxMaxLength = 39,
-				Value = false,
-			},
-			new()
-			{
-				PluginOptionType = PluginAdditionalOption.AdditionalOptionType.Textbox,
-				Key = AuthToken,
-				DisplayLabel = Resources.plugin_auth_token,
-				Value = false,
-			},
-		];
-
-		public void UpdateSettings(PowerLauncherPluginSettings settings)
+	public IEnumerable<PluginAdditionalOption> AdditionalOptions =>
+	[
+		new()
 		{
-			_defaultUser = settings?.AdditionalOptions?.FirstOrDefault(x => x.Key == DefaultUser)?.TextValue ?? string.Empty;
-			// TODO: how to hide the auth token in settings?
-			_authToken = settings?.AdditionalOptions?.FirstOrDefault(x => x.Key == AuthToken)?.TextValue ?? string.Empty;
-			GitHub.UpdateAuthSetting(_authToken);
+			PluginOptionType = PluginAdditionalOption.AdditionalOptionType.Textbox,
+			Key = DefaultUser,
+			DisplayLabel = Resources.plugin_default_user,
+			// Max length of a GitHub username is 39
+			TextBoxMaxLength = 39,
+			Value = false,
+		},
+		new()
+		{
+			PluginOptionType = PluginAdditionalOption.AdditionalOptionType.Textbox,
+			Key = AuthToken,
+			DisplayLabel = Resources.plugin_auth_token,
+			Value = false,
+		},
+	];
+
+	public void UpdateSettings(PowerLauncherPluginSettings settings)
+	{
+		_defaultUser = settings?.AdditionalOptions?.FirstOrDefault(x => x.Key == DefaultUser)?.TextValue ?? string.Empty;
+		// TODO: how to hide the auth token in settings?
+		_authToken = settings?.AdditionalOptions?.FirstOrDefault(x => x.Key == AuthToken)?.TextValue ?? string.Empty;
+		GitHub.UpdateAuthSetting(_authToken);
+	}
+
+	// handle user repo user
+	public List<Result> Query(Query query)
+	{
+		ArgumentNullException.ThrowIfNull(query);
+
+		var search = query.Search;
+
+		// empty query
+		if (string.IsNullOrEmpty(search))
+		{
+			var arguments = "github.com";
+			return
+			[
+				new Result
+				{
+					Title = EmptyDescription,
+					SubTitle = string.Format(CultureInfo.CurrentCulture, PluginInBrowserName, BrowserInfo.Name ?? BrowserInfo.MSEdgeName),
+					QueryTextDisplay = string.Empty,
+					IcoPath = _icon,
+					ProgramArguments = arguments,
+					Action = action => Helper.OpenCommandInShell(BrowserInfo.Path, BrowserInfo.ArgumentsPattern, arguments),
+				}
+			];
 		}
 
-		// handle user repo user
-		public List<Result> Query(Query query)
+		// delay execution for repo query
+		if (!search.Contains('/'))
 		{
-			ArgumentNullException.ThrowIfNull(query);
+			throw new OperationCanceledException();
+		}
 
-			var search = query.Search;
+		List<GitHubRepo> repos;
+		string target;
 
-			// empty query
-			if (string.IsNullOrEmpty(search))
+		if (search.StartsWith('/'))
+		{
+			if (string.IsNullOrEmpty(_defaultUser))
 			{
-				var arguments = "github.com";
 				return
 				[
 					new Result
 					{
-						Title = EmptyDescription,
-						SubTitle = string.Format(CultureInfo.CurrentCulture, PluginInBrowserName, BrowserInfo.Name ?? BrowserInfo.MSEdgeName),
+						Title = Resources.plugin_default_user_not_set,
+						SubTitle = Resources.plugin_default_user_not_set_description,
 						QueryTextDisplay = string.Empty,
 						IcoPath = _icon,
-						ProgramArguments = arguments,
-						Action = action =>
-						{
-							if (!Helper.OpenCommandInShell(BrowserInfo.Path, BrowserInfo.ArgumentsPattern, arguments))
-							{
-								return false;
-							}
-
-							return true;
-						},
+						Action = action => true,
 					}
 				];
 			}
 
-			// delay execution for repo query
-			if (!search.Contains('/'))
-			{
-				throw new OperationCanceledException();
-			}
+			var cacheKey = _defaultUser;
+			target = search[1..];
 
-			List<GitHubRepo> repos;
-			string target;
+			repos = _cache.GetOrAdd(cacheKey, () => DefaultUserRepoQuery(cacheKey));
+		}
+		else
+		{
+			var split = search.Split('/', 2);
 
-			if (search.StartsWith('/'))
-			{
-				if (string.IsNullOrEmpty(_defaultUser))
-				{
-					return
-					[
-						new Result
-						{
-							Title = Resources.plugin_default_user_not_set,
-							SubTitle = Resources.plugin_default_user_not_set_description,
-							QueryTextDisplay = string.Empty,
-							IcoPath = _icon,
-							Action = action => true,
-						}
-					];
-				}
+			var cacheKey = split[0];
+			target = split[1];
 
-				var cacheKey = _defaultUser;
-				target = search[1..];
-
-				repos = _cache.GetOrAdd(cacheKey, () => DefaultUserRepoQuery(cacheKey));
-			}
-			else
-			{
-				var split = search.Split('/', 2);
-
-				var cacheKey = split[0];
-				target = split[1];
-
-				repos = _cache.GetOrAdd(cacheKey, () => UserRepoQuery(cacheKey));
-			}
-
-			List<Result> results = repos.ConvertAll(repo =>
-			{
-				MatchResult match = StringMatcher.FuzzySearch(target, repo.FullName.Split('/', 2)[1]);
-				return new Result
-				{
-					Title = repo.FullName,
-					SubTitle = repo.Description,
-					QueryTextDisplay = repo.FullName,
-					IcoPath = repo.Fork ? _iconFork : _iconRepo,
-					Score = match.Score,
-					ContextData = new ResultData(repo.HtmlUrl),
-					Action = action =>
-					{
-						if (!Helper.OpenCommandInShell(BrowserInfo.Path, BrowserInfo.ArgumentsPattern, repo.HtmlUrl))
-						{
-							return false;
-						}
-
-						return true;
-					},
-				};
-			});
-
-			//results = results.Where(r => r.Title.StartsWith(target, StringComparison.OrdinalIgnoreCase)).ToList();
-			return results;
-
-			static List<GitHubRepo> UserRepoQuery(string user) => GitHub.UserRepoQuery(user).Result.Match(
-					ok: r => r,
-					err: e => [new(e.GetType().Name, string.Empty, e.Message, false)]);
-
-			static List<GitHubRepo> DefaultUserRepoQuery(string user) => GitHub.DefaultUserRepoQuery(user).Result.Match(
-					ok: r => r,
-					err: e => [new(e.GetType().Name, string.Empty, e.Message, false)]);
+			repos = _cache.GetOrAdd(cacheKey, () => UserRepoQuery(cacheKey));
 		}
 
-		// handle repo search with delay
-		public List<Result> Query(Query query, bool delayedExecution)
+		var results = repos.ConvertAll(repo =>
 		{
-			if (!delayedExecution || query.Search.Contains('/') || string.IsNullOrWhiteSpace(query.Search))
+			MatchResult match = StringMatcher.FuzzySearch(target, repo.FullName.Split('/', 2)[1]);
+			return new Result
 			{
-				throw new OperationCanceledException();
-			}
+				Title = repo.FullName,
+				SubTitle = repo.Description,
+				QueryTextDisplay = repo.FullName,
+				IcoPath = repo.Fork ? _iconFork : _iconRepo,
+				Score = match.Score,
+				ContextData = new ResultData(repo.HtmlUrl),
+				Action = action => Helper.OpenCommandInShell(BrowserInfo.Path, BrowserInfo.ArgumentsPattern, repo.HtmlUrl),
+			};
+		}).Where(r => r.Score > 0).ToList();
 
-			return RepoQuery(query.Search).ConvertAll(repo => new Result
+		return results;
+
+		static List<GitHubRepo> UserRepoQuery(string user) => GitHub.UserRepoQuery(user).Result.Match(
+				ok: r => r,
+				err: e => [new(e.GetType().Name, string.Empty, e.Message, false)]);
+
+		static List<GitHubRepo> DefaultUserRepoQuery(string user) => GitHub.DefaultUserRepoQuery(user).Result.Match(
+				ok: r => r,
+				err: e => [new(e.GetType().Name, string.Empty, e.Message, false)]);
+	}
+
+	// handle repo search with delay
+	public List<Result> Query(Query query, bool delayedExecution)
+	{
+		return !delayedExecution || query.Search.Contains('/') || string.IsNullOrWhiteSpace(query.Search)
+			? throw new OperationCanceledException()
+			: RepoQuery(query.Search).ConvertAll(repo => new Result
 			{
 				Title = repo.FullName,
 				SubTitle = repo.Description,
 				QueryTextDisplay = repo.FullName,
 				IcoPath = repo.Fork ? _iconFork : _iconRepo,
 				ContextData = new ResultData(repo.HtmlUrl),
-				Action = action =>
-				{
-					if (!Helper.OpenCommandInShell(BrowserInfo.Path, BrowserInfo.ArgumentsPattern, repo.HtmlUrl))
-					{
-						return false;
-					}
-
-					return true;
-				},
+				Action = action => Helper.OpenCommandInShell(BrowserInfo.Path, BrowserInfo.ArgumentsPattern, repo.HtmlUrl),
 			});
 
-			static List<GitHubRepo> RepoQuery(string search) => GitHub.RepoQuery(search).Result.Match(
-					ok: r => r.Items,
-					err: e => [new(e.GetType().Name, string.Empty, e.Message, false)]);
+		static List<GitHubRepo> RepoQuery(string search) => GitHub.RepoQuery(search).Result.Match(
+				ok: r => r.Items,
+				err: e => [new(e.GetType().Name, string.Empty, e.Message, false)]);
+	}
+
+	public List<ContextMenuResult> LoadContextMenus(Result selectedResult)
+	{
+		if (selectedResult.ContextData is not ResultData selectedData)
+		{
+			return [];
 		}
 
-		public List<ContextMenuResult> LoadContextMenus(Result selectedResult)
-		{
-			if (selectedResult.ContextData is not ResultData selectedData)
+		var url = selectedData.Url;
+		var issue = $"{url}/issues";
+		var pr = $"{url}/pulls";
+		return [
+			new ContextMenuResult
 			{
-				return [];
+				PluginName = Name,
+				Title = Resources.plugin_open_issues,
+				Glyph = "\xE958",
+				FontFamily = "Segoe Fluent Icons,Segoe MDL2 Assets",
+				AcceleratorKey = Key.I,
+				AcceleratorModifiers = ModifierKeys.Control,
+				Action = _ => Helper.OpenCommandInShell(BrowserInfo.Path, BrowserInfo.ArgumentsPattern, issue),
+			},
+			new ContextMenuResult
+			{
+				PluginName = Name,
+				Title = Resources.plugin_open_pull_requests,
+				Glyph = "\xF003",
+				FontFamily = "Segoe Fluent Icons,Segoe MDL2 Assets",
+				AcceleratorKey = Key.P,
+				AcceleratorModifiers = ModifierKeys.Control,
+				Action = _ => Helper.OpenCommandInShell(BrowserInfo.Path, BrowserInfo.ArgumentsPattern, pr),
+			},
+		];
+	}
+
+	public void Init(PluginInitContext context)
+	{
+		_context = context ?? throw new ArgumentNullException(nameof(context));
+		_context.API.ThemeChanged += OnThemeChanged;
+		_cache = new CachingService();
+		_cache.DefaultCachePolicy.DefaultCacheDurationSeconds = (int)TimeSpan.FromMinutes(1).TotalSeconds;
+		UpdateIconPath(_context.API.GetCurrentTheme());
+		BrowserInfo.UpdateIfTimePassed();
+	}
+
+	public string GetTranslatedPluginTitle() => Resources.plugin_name;
+
+	public string GetTranslatedPluginDescription() => Resources.plugin_description;
+
+	private void OnThemeChanged(Theme oldTheme, Theme newTheme) => UpdateIconPath(newTheme);
+
+	private void UpdateIconPath(Theme theme)
+	{
+		_iconFolderPath = theme is Theme.Light or Theme.HighContrastWhite ? "Images\\light" : "Images\\dark";
+		_icon = $"{_iconFolderPath}\\GitHub.png";
+		_iconRepo = $"{_iconFolderPath}\\Repo.png";
+		_iconFork = $"{_iconFolderPath}\\Fork.png";
+	}
+
+	public Control CreateSettingPanel() => throw new NotImplementedException();
+
+	public void ReloadData()
+	{
+		if (_context is null)
+		{
+			return;
+		}
+
+		UpdateIconPath(_context.API.GetCurrentTheme());
+		BrowserInfo.UpdateIfTimePassed();
+	}
+
+	public void Dispose()
+	{
+		Dispose(true);
+		GC.SuppressFinalize(this);
+	}
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!_disposed && disposing)
+		{
+			if (_context != null && _context.API != null)
+			{
+				_context.API.ThemeChanged -= OnThemeChanged;
 			}
 
-			var url = selectedData.Url;
-			var issue = $"{url}/issues";
-			var pr = $"{url}/pulls";
-			return [
-				new ContextMenuResult
-				{
-					PluginName = Name,
-					Title = Resources.plugin_open_issues,
-					Glyph = "\xE958",
-					FontFamily = "Segoe Fluent Icons,Segoe MDL2 Assets",
-					AcceleratorKey = Key.I,
-					AcceleratorModifiers = ModifierKeys.Control,
-					Action = _ =>
-					{
-						if (!Helper.OpenCommandInShell(BrowserInfo.Path, BrowserInfo.ArgumentsPattern, issue))
-						{
-							return false;
-						}
-
-						return true;
-					},
-				},
-				new ContextMenuResult
-				{
-					PluginName = Name,
-					Title = Resources.plugin_open_pull_requests,
-					Glyph = "\xF003",
-					FontFamily = "Segoe Fluent Icons,Segoe MDL2 Assets",
-					AcceleratorKey = Key.P,
-					AcceleratorModifiers = ModifierKeys.Control,
-					Action = _ =>
-					{
-						if (!Helper.OpenCommandInShell(BrowserInfo.Path, BrowserInfo.ArgumentsPattern, pr))
-						{
-							return false;
-						}
-
-						return true;
-					},
-				},
-			];
-		}
-
-		public void Init(PluginInitContext context)
-		{
-			_context = context ?? throw new ArgumentNullException(nameof(context));
-			_context.API.ThemeChanged += OnThemeChanged;
-			_cache = new CachingService();
-			_cache.DefaultCachePolicy.DefaultCacheDurationSeconds = (int)TimeSpan.FromMinutes(1).TotalSeconds;
-			UpdateIconPath(_context.API.GetCurrentTheme());
-			BrowserInfo.UpdateIfTimePassed();
-		}
-
-		public string GetTranslatedPluginTitle()
-		{
-			return Resources.plugin_name;
-		}
-
-		public string GetTranslatedPluginDescription()
-		{
-			return Resources.plugin_description;
-		}
-
-		private void OnThemeChanged(Theme oldTheme, Theme newTheme)
-		{
-			UpdateIconPath(newTheme);
-		}
-
-		private void UpdateIconPath(Theme theme)
-		{
-			_iconFolderPath = theme is Theme.Light or Theme.HighContrastWhite ? "Images\\light" : "Images\\dark";
-			_icon = $"{_iconFolderPath}\\GitHub.png";
-			_iconRepo = $"{_iconFolderPath}\\Repo.png";
-			_iconFork = $"{_iconFolderPath}\\Fork.png";
-		}
-
-		public Control CreateSettingPanel()
-		{
-			throw new NotImplementedException();
-		}
-
-		public void ReloadData()
-		{
-			if (_context is null)
-			{
-				return;
-			}
-
-			UpdateIconPath(_context.API.GetCurrentTheme());
-			BrowserInfo.UpdateIfTimePassed();
-		}
-
-		public void Dispose()
-		{
-			Dispose(true);
-			GC.SuppressFinalize(this);
-		}
-
-		protected virtual void Dispose(bool disposing)
-		{
-			if (!_disposed && disposing)
-			{
-				if (_context != null && _context.API != null)
-				{
-					_context.API.ThemeChanged -= OnThemeChanged;
-				}
-
-				_disposed = true;
-			}
+			_disposed = true;
 		}
 	}
 }
